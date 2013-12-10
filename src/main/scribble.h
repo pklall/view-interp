424255850d5f4ac797ead513bcae52caee4ddc1f
Puneet Lall <puneetlall@gmail.com>
2013-12-09 19:49:06 -0500

    Added scribble-acquisition function

---
+++ src/main/scribble.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "common.h"
+
+/**
+ * Displays a UI for acquiring "scribbles" from the user.
+ *
+ * Possible scribble colors are indicated by the colors present
+ * in palette, which must be a row of colors.
+ *
+ * The resulting scribbles are stored as indices into the palette
+ * with -1 indicating no scribble.
+ */
+void displayUserScribbles(
+        const CImg<float>& background,
+        const CImg<float>& palette,
+        CImg<int>& scribbles,
+        function<void()> afterStrokeCallback);
